fa439ee24d0ea66c07bfc973dee8e2b0c528c48e
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-01-20 21:31:50 +0530

    Create to check no.c

---
+++ to check no.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+	 int k,l,m[10],i,count=0;
+	 printf("get the numbers\n");
+	 for(i=0;i<5;i++)
+	 {
+	 scanf("%d",&m[10]);
+	}
+	printf("enter the number to verify\n");
+	scanf("%d %d",&k,&l);
+
+		if((k==m[i])||(l==m[i]))
+		for(i=0;i<5;i++)
+		{
+		{
+			count++;
+			
+		}
+		
+	}
+	if(count=5)
+	{
+		printf("no is present");
+		
+	}
+	else
+		{
+			printf("no is not present");
+		}
+	return 0;
+}
+ 
