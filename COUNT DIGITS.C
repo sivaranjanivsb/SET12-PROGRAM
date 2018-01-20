45bcc725e420dd510b4891f98c3e7ac4a7672ef1
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-01-20 20:06:15 +0530

    Create COUNT DIGITS.C

---
+++ COUNT DIGITS.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int no,rem,count;
+	printf("enter the number");
+	scanf("%d",&no);
+	
+	while(no!=0)
+	{
+	rem=no%10;
+	count++;
+	no=no/10;
+	}
+	printf("the number of digits in a number is %d",count);
+	return 0;
+}
+
