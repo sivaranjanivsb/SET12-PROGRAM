3073435c5d4829108470ac44707555b5c1422176
sivaranjanivsb <31504326+sivaranjanivsb@users.noreply.github.com>
2018-01-21 13:34:10 +0530

    Update to check no.c

--- to check no.c
+++ to check no.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main(void) {
-	 int k,l,m[10],i,count=0;
+	 int k,l,m[5],i,count=0;
 	 printf("get the numbers\n");
 	 for(i=0;i<5;i++)
 	 {
-	 scanf("%d",&m[10]);
+	 scanf("%d",&m[5]);
 	}
 	printf("enter the number to verify\n");
 	scanf("%d %d",&k,&l);
-
-		if((k==m[i])||(l==m[i]))
-		for(i=0;i<5;i++)
+	for(i=0;i<5;i++)
 		{
+	
+		while((k=m[i])||(l=m[i]))
 		{
 			count++;
 			
 		}
 		
 	}
-	if(count=5)
+	if(count>=1)
 	{
 		printf("no is present");
 		
